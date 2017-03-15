cdd4379d6b881ca9ccd4c10f50cd0698cc69ff68
Regretful123 <jordbej@yahoo.com>
2017-03-14 20:42:51 -0500

    update project
    Update project

--- Assets/MovieStart.cs
+++ Assets/MovieStart.cs
@@ -4,20 +4,18 @@ using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(RawImage))]
 public class MovieStart : MonoBehaviour {
-    private RawImage movieObj;
-    private MovieTexture movieTex;
+    public MovieTexture movieTex;
     public int SceneToLoad = 1;
 
 	// Use this for initialization
 	void Start () {
-        movieObj = GetComponent<RawImage>();
-        movieTex = (MovieTexture)movieObj.mainTexture;
         // only use this script if it has movie texture assigned to this script
         if (movieTex == null) return;
         // play the movie when possible
         movieTex.Play();
         // invoke a call after the movie is done.
-        Invoke("ChangeScene", movieTex.duration);
+        Debug.Log(movieTex.duration);
+        //Invoke("ChangeScene", movieTex.duration);
 	}
 	
     // change the scene
