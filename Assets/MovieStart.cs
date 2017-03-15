bd615362d4920458b7687b54a7351a954c8b05d2
Regretful123 <jordbej@yahoo.com>
2017-03-14 23:28:33 -0500

    Update files
    Update files

--- Assets/MovieStart.cs
+++ Assets/MovieStart.cs
@@ -1,23 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
-[RequireComponent(typeof(RawImage))]
+[RequireComponent(typeof(RawImage))]    
 public class MovieStart : MonoBehaviour {
-    public MovieTexture movieTex;
+    //public MovieTexture movieTex;
     public int SceneToLoad = 1;
+    public float timetowait = 1.03f;
+    private float i = 0;
 
 	// Use this for initialization
 	void Start () {
         // only use this script if it has movie texture assigned to this script
-        if (movieTex == null) return;
+        //if (movieTex == null) return;
         // play the movie when possible
-        movieTex.Play();
+        //movieTex.Play();
         // invoke a call after the movie is done.
-        Debug.Log(movieTex.duration);
+        //float t = movieTex.duration;
+        //Debug.Log(t);
         //Invoke("ChangeScene", movieTex.duration);
-	}
-	
+
+    }
+
+    private void FixedUpdate()
+    {
+        i += Time.fixedDeltaTime;
+        if ( i > timetowait )
+        {
+            ChangeScene();
+            i = 0;
+        }
+    }
+
     // change the scene
     void ChangeScene()
     {
